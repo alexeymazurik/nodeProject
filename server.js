311f7754c294c532dd659727b6970cf1a2158bf8
Alexey Mazurik <alexeymazurik@gmail.com>
2014-07-02 14:10:50 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+function start(route, handle) {
+    function onRequest(request,response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        var content = route(handle, pathname);
+
+        response.writeHead(200);
+        response.write(content);
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log('Server has started at 8888');
+}
+
+exports.start = start;
